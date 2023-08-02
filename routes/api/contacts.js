29c424fe056ec42f15c0451c58d5384ea180d41f
MaggieWesolowska <maggiewes07@gmail.com>
2023-08-02 22:18:38 +0200

    edited routing commands

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -12,34 +12,27 @@ const router = express.Router();
 const schema = require('../../models/contacts-schema.js');
 
 router.get('/', async (req, res) => {
-  const contacts = await listContacts();
-  res.json(contacts);
+  const result = await listContacts();
+  res.status(200).json(result);
 });
 
 router.get('/:contactId', async (req, res) => {
-  const contact = await getContactById(
-    req.params.contactId
-  );
-  if (!contact) {
+  const result = await getContactById(req.params.contactId);
+  if (!result) {
     res.status(404).json({ message: 'Contact not found' });
   }
-  res.json(contact);
+  res.status(200).json(result);
 });
 
 router.post('/', async (req, res) => {
-  const newContact = {
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-  };
-  const validate = schema.validate(newContact);
+  const validate = schema.validate(req.body);
   if (validate.error) {
     res
       .status(400)
       .json({ message: validate.error.message });
   } else {
-    const addedContact = await addContact(newContact);
-    res.json(addedContact);
+    const result = await addContact(req.body);
+    res.status(201).json(result);
   }
 });
 
@@ -50,27 +43,22 @@ router.delete('/:contactId', async (req, res) => {
   if (!deleteContact) {
     res.status(404).json({ message: 'Contact not found' });
   }
-  res.json(deleteContact);
+  res.status(200).json({ message: 'Contact deleted' });
 });
 
-router.put('/:contactId', async (req, res, next) => {
-  const newContact = {
-    name: req.body.name,
-    phone: req.body.phone,
-    email: req.body.email,
-  };
-  const validate = schema.validate(newContact);
+router.put('/:contactId', async (req, res) => {
+  const validate = schema.validate(req.body);
   if (validate.error) {
     res
       .status(400)
       .json({ message: validate.error.message });
   } else {
-    const updatedContact = await updateContact(
+    const result = await updateContact(
       req.params.contactId,
-      newContact
+      req.body
     );
-    if (updatedContact) {
-      res.json(updatedContact);
+    if (result) {
+      res.json(result);
     } else {
       res
         .status(404)
