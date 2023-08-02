d6a30a5f194fbf42a254847124f87fac00c0096b
MaggieWesolowska <maggiewes07@gmail.com>
2023-08-02 19:06:14 +0200

    edited contacts, removed unused code

--- models/contacts-schema.js
+++ models/contacts-schema.js
@@ -2,14 +2,18 @@ const Joi = require('joi');
 
 const schema = Joi.object({
   name: Joi.string().required().messages({
-    'any.required': 'name is required',
+    'string.base': `"name" should be a type of 'string'`,
+    'any.required': `"name" is a required field`,
   }),
   phone: Joi.string().required().messages({
-    'any.required': 'phone is required',
+    'string.base': `"phone" should be a type of 'string'`,
+    'any.required': `"phone" is a required field`,
   }),
   email: Joi.string().email().required().messages({
-    'any.required': 'email is required',
+    'string.base': `"email" should be a type of 'string'`,
+    'any.required': `"email" is a required field`,
   }),
+  favorite: Joi.boolean(),
 });
 
 module.exports = schema;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,13 +4,13 @@ const {
   addContact,
   removeContact,
   updateContact,
-} = require('../../models/contacts');
-const Joi = require('joi');
+} = require('../../models/contacts.js');
+// const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
-const schema = require('../../models/contacts-schema');
+const schema = require('../../models/contacts-schema.js');
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
   const contacts = await listContacts();
   res.json(contacts);
 });
@@ -25,7 +25,7 @@ router.get('/:contactId', async (req, res) => {
   res.json(contact);
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
   const newContact = {
     name: req.body.name,
     phone: req.body.phone,
@@ -33,35 +33,9 @@ router.post('/', async (req, res, next) => {
   };
   const validate = schema.validate(newContact);
   if (validate.error) {
-    const validateMissing = () => {
-      const key = Object.keys(
-        validate.error._original
-      ).find(key => {
-        return validate.error._original[key] === undefined;
-      });
-      if (key) {
-        return true;
-      }
-      return false;
-    };
-    const isMissing = validateMissing();
-
-    if (isMissing) {
-      const missingKeys = [];
-      Object.keys(validate.error._original).forEach(key => {
-        if (!validate.error._original[key]) {
-          missingKeys.push(key);
-        }
-      });
-      const message =
-        'Required fields missing: ' +
-        missingKeys.join(', ');
-      res.status(400).json({ message: message });
-    } else {
-      res
-        .status(400)
-        .json({ message: validate.error.message });
-    }
+    res
+      .status(400)
+      .json({ message: validate.error.message });
   } else {
     const addedContact = await addContact(newContact);
     res.json(addedContact);
@@ -86,35 +60,9 @@ router.put('/:contactId', async (req, res, next) => {
   };
   const validate = schema.validate(newContact);
   if (validate.error) {
-    const validateMissing = () => {
-      const key = Object.keys(
-        validate.error._original
-      ).find(key => {
-        return validate.error._original[key] === undefined;
-      });
-      if (key) {
-        return true;
-      }
-      return false;
-    };
-    const isMissing = validateMissing();
-
-    if (isMissing) {
-      const missingKeys = [];
-      Object.keys(validate.error._original).forEach(key => {
-        if (!validate.error._original[key]) {
-          missingKeys.push(key);
-        }
-      });
-      const message =
-        'Required fields missing: ' +
-        missingKeys.join(', ');
-      res.status(400).json({ message: message });
-    } else {
-      res
-        .status(400)
-        .json({ message: validate.error.message });
-    }
+    res
+      .status(400)
+      .json({ message: validate.error.message });
   } else {
     const updatedContact = await updateContact(
       req.params.contactId,
