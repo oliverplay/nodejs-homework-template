566e712c2ca620191e9bf4632db4734c58ade4b2
James Lynch <jimlynch268@gmail.com>
2023-08-04 15:27:05 -0400

    start setting up mongoose

--- models/contacts.js
+++ models/contacts.js
@@ -1,101 +1,24 @@
-const fs = require('fs/promises');
-const  path = require('path');
-const contactsPath = path.join(__dirname, 'contacts.json');
+const { Schema, model } = require('mongoos');
 
+const ContactSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+email: {
+    type: String,
+    required: true,
+  },
+phone: {
+    type: Number,
+    required: true,
+  },
+  favorie: {
+    type: Boolean,
+    required: false,
+  },
+})
 
-const listContacts = async () => {
- try {
-    const data = await fs.readFile(contactsPath);
-    return JSON.parse(data);
-  } catch (error) {
-    return { error }
-  }
-}
+const Contact = model('contacts', ContactSchema)
 
-const getContactById = async (contactId) => {
- try {
-   const data = await fs.readFile(contactsPath);
-   const jsonDate = JSON.parse(data);
-   const findUser = jsonDate.find(item => item.id === contactId)
-           if (!findUser) {
-      const message = `Not Found`;
-      return { message };
-    } else {
-      return { findUser };
-    }
-  } catch (error) {
-    return { error }
-  }
-
-}
-
-const removeContact = async (contactId) => {
-  try {
-    const data = await fs.readFile(contactsPath);
-   const jsonDate = JSON.parse(data);
-   const findUser = jsonDate.find(item => item.id === contactId)
-           if (!findUser) {
-      const message = `Not Found`;
-      return { message };
-           } else {
-        const newList = jsonDate.filter(item => item.id !== contactId)
-        await fs.writeFile(contactsPath, JSON.stringify(newList))     
-         const message = `contact deleted`;    
-         return { message };    
-    }
-  } catch (error) {
-    return { error }
-  }
-
-}
-
-const addContact = async (body) => {
-  try {
-const data = await fs.readFile(contactsPath) 
-  const jsonDate = JSON.parse(data)
-  jsonDate.push(body)
-    await fs.writeFile(contactsPath, JSON.stringify(jsonDate))
-    const addedContact = body
-  return { addedContact };  
-    }
-   catch (error) {
-    return { error }
-  }
-}
-
-const updateContact = async (contactId, body) => {
-  const {name, email, phone} = body
-  try {
-  const data = await fs.readFile(contactsPath) 
-    const jsonDate = JSON.parse(data)
-
-    const findUser = jsonDate.findIndex(item => item.id === contactId)
-           if (findUser === -1) {
-      const message = `Not Found`;
-      return { message };
-           } else {
-             jsonDate[findUser] = { ...jsonDate[findUser], name, email, phone }
-        
-             await fs.writeFile(contactsPath, JSON.stringify(jsonDate))
-    const updateContact = jsonDate[findUser]
-  return { updateContact };  
-    }
-    
-  }
-  catch (error) {
-    return { error }
-  }
-
-
-
-
-
-}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+module.exports = Contact
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require("./app");
+const express = require('express');
+const db = require('./config');
+const routes = require('./routes');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use('/api', routes);
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log('Server is listening at port 3000');
+  });
 });
