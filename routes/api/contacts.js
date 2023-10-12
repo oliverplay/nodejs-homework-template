7620a744f256e9cc83447b374839c4ea2fb4262e
Katniss-Nataliia <natko.boiko@gmail.com>
2023-10-11 22:36:48 -0600

    REST

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,46 @@
 const express = require('express')
-
+const contacts = require('../../models/contacts.json');
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json(contacts);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const id = req.params.contactId;
+  const contact = contacts.find(contact => contact.id === id);
+  if(!contact){
+    res.status(400).json({message: "contact not found"})
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {id} = req.params;
+  const {name, email, phone} = req.body;
+  const newContact = {
+    id,
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  res.json({contacts})
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId} = req.params;
+  const contact = contacts.filter(el => el.id !== contactId);  
+  res.json({contact});
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId} = req.params;
+  const {name, email, phone} = req.body;
+  const updatedContact = contacts.filter(contact => contact.id === contactId);
+  updatedContact.name = name;
+  updatedContact.email = email;
+  updatedContact.phone = phone;
+  res.json({updatedContact})
 })
 
 module.exports = router
