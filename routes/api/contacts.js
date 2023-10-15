b67838c4a856e0f8e3739961f5364103c8bfe06f
Katniss-Nataliia <natko.boiko@gmail.com>
2023-10-14 22:04:24 -0600

    applying the remarks

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,36 +1,52 @@
 const express = require('express')
 const contacts = require('../../models/contacts.json');
-const router = express.Router()
+const router = express.Router();
+const {v4} = require("uuid");
 
 router.get('/', async (req, res, next) => {
-  res.json(contacts);
+  res.json({
+    status:'success',
+    code:200,
+    data:{
+      contacts,
+    },
+  });
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  const id = req.params.contactId;
-  const contact = contacts.find(contact => contact.id === id);
+  const {contactId} = req.params;
+  const [contact] = contacts.filter(contact => contact.id === contactId);
   if(!contact){
     res.status(400).json({message: "contact not found"})
   }
+  res.json({
+    status:'success',
+    code:200,
+    data:{contact}
+  })
 })
 
 router.post('/', async (req, res, next) => {
-  const {id} = req.params;
   const {name, email, phone} = req.body;
   const newContact = {
-    id,
+    id: v4(),
     name,
     email,
     phone,
   };
   contacts.push(newContact);
-  res.json({contacts})
+  res.status(201).json({
+    status:'success',
+    code:201,
+    data:{newContact}
+  })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
   const {contactId} = req.params;
-  const contact = contacts.filter(el => el.id !== contactId);  
-  res.json({contact});
+  const contact = contacts.filter(el => el.id !== contactId); 
+  contacts =[...contact] 
+  res.status(204).json();
 })
 
 router.put('/:contactId', async (req, res, next) => {
@@ -40,7 +56,11 @@ router.put('/:contactId', async (req, res, next) => {
   updatedContact.name = name;
   updatedContact.email = email;
   updatedContact.phone = phone;
-  res.json({updatedContact})
+  res.json({
+    status:'success',
+    code:200,
+    data:{updatedContact}
+  })
 })
 
 module.exports = router
