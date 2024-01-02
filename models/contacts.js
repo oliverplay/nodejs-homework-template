2bdb58dfc74a5cebe91bf5082f46da246ed986fb
19Aleksandra95 <polar.heder@gmail.com>
2024-01-02 19:16:52 +0100

    fixed staff

--- models/contacts.js
+++ models/contacts.js
@@ -2,6 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 const { nanoid }= require("nanoid");
 
+
 const contactPath = path.join(__dirname, "contacts.json")
 
 const listContacts = async () => {
