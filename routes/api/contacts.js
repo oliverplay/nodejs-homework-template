1e12da7f52132328eea650439f947565f1f2706e
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-08-21 19:01:50 +0800

    app.js update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,22 @@
-const express = require('express')
+const express = require("expr;ess");
+const fs = require("fs");
+const path = require("path");
+const router = express.Router();
 
-const router = express.Router()
+let contactsPath = "";
+router.use((req, res, next) => {
+  if (!contactsPath) {
+    contactsPath = req.app.locals.contactsPath;
+  }
+  next();
+});
+router.get("/", (req, res, next) => {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    res.json(JSON.parse(data));
+  });
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
