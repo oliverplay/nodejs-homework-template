3dbeee3c946213bd2b5fd6fb6b45b318bd4a061d
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-27 08:35:04 +0800

    Update validateContact.js

--- helpers/validateContact.js
+++ helpers/validateContact.js
@@ -1,1 +1,15 @@
+// helpers/validateContact.js
+const Joi = require('joi');
+
+const validateContact = (contact) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required()
+  });
+
+  return schema.validate(contact);
+};
+
+module.exports = validateContact;
 
