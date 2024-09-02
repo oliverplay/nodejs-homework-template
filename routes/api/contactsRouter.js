206a6b32d308bd7b36523d029b86a026df4a8ee3
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-09-02 19:38:50 +0800

    update

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,21 +1,42 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from "express";
+import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
+// prettier-ignore
+import { addContact, deleteContactById, getAllContacts, getContactById, updateContactById, updateStatusContact } from "../../controllers/contactsController.js";
 
-import { app } from "./app.js";
+const router = express.Router();
 
-dotenv.config();
+/* GET: // http://localhost:3000/api/contacts */
+router.get("/", ctrlWrapper(getAllContacts));
 
-const { DB_HOST, PORT = 3000 } = process.env;
+/* GET: // http://localhost:3000/api/contacts/:contactId */
+router.get("/:contactId", ctrlWrapper(getContactById));
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT, () =>
-      console.log(`Server running. Use our API on port: ${PORT}`)
-    );
-    console.log("Database connect successful");
-  })
-  .catch((err) => {
-    console.log(`Server not running. Error message: ${err.message}`);
-    process.exit(1);
-  });
+/* POST: // http://localhost:3000/api/contacts/ 
+{
+    "name": "Marvin Pacis",
+    "email": "marvinpacis@example.com",
+    "phone": "(639) 840-6611"
+} 
+*/
+router.post("/", ctrlWrapper(addContact));
+
+/* DELETE: // http://localhost:3000/api/contacts/:contactId */
+router.delete("/:contactId", ctrlWrapper(deleteContactById));
+
+/* PUT: // http://localhost:3000/api/contacts/:contactId 
+{
+    "name": "Joanna Shaw",
+    "email": "joannashaw@example.com",
+    "phone": "(639) 777-8819"
+} 
+*/
+router.put("/:contactId", ctrlWrapper(updateContactById));
+
+/* PATCH: // http://localhost:3000/api/contacts/:contactId/favorite
+{
+    "favorite": true,
+}
+*/
+router.patch("/:contactId/favorite", ctrlWrapper(updateStatusContact));
+
+export { router };
