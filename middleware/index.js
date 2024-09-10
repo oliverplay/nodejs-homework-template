f7c79217fe01f1e1a97bf47debf7e2a37d55aca4
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-09-10 14:27:51 +0800

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,1 +1,5 @@
+const isValidId = require("./isValidId");
+const validateBody = require("./validateBody");
+const handleMongooseError = require("./handleMongooseError");
 
+module.exports = { isValidId, validateBody, handleMongooseError };
