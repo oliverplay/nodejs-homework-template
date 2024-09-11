ba76205894ee93d84ce34ac43774b55b50388ea8
valeeriiiee <obleavalerie@gmail.com>
2024-09-12 02:08:06 +0800

    added comment = end routes

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -2,22 +2,38 @@ import express from "express";
 
 const router = express.Router();
 
+// GET: // http://localhost:3000/api/contacts
 router.get("/", async (req, res, next) => {
   res.json({ message: "template message" });
 });
 
+// GET: // http://localhost:3000/api/contacts/1
 router.get("/:contactId", async (req, res, next) => {
   res.json({ message: "template message" });
 });
 
+/* POST: // http://localhost:3000/api/contacts/ 
+{
+    "id": 3,
+    "name": "Marvin Pacis",
+    "email": "marvinpacis@example.com"
+} 
+*/
 router.post("/", async (req, res, next) => {
   res.json({ message: "template message" });
 });
 
+// DELETE: // http://localhost:3000/api/contacts/1
 router.delete("/:contactId", async (req, res, next) => {
   res.json({ message: "template message" });
 });
 
+/* PUT: // http://localhost:3000/api/contacts/1
+{
+    "name": "Joanna Shaw",
+    "email": "shaw@example.com"
+} 
+*/
 router.put("/:contactId", async (req, res, next) => {
   res.json({ message: "template message" });
 });
