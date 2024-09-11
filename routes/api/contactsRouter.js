114c73fa9f69bf34509e9e37bfd60a0be51e2836
valeeriiiee <obleavalerie@gmail.com>
2024-09-12 02:26:21 +0800

    Update contactsRouter.js

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -3,12 +3,12 @@ import express from "express";
 const router = express.Router();
 
 // GET: // http://localhost:3000/api/contacts
-router.get("/", async (req, res, next) => {
+router.get("/", async (_req, res, next) => {
   res.json({ message: "template message" });
 });
 
 // GET: // http://localhost:3000/api/contacts/1
-router.get("/:contactId", async (req, res, next) => {
+router.get("/:contactId", async (_req, res, next) => {
   res.json({ message: "template message" });
 });
 
@@ -19,12 +19,12 @@ router.get("/:contactId", async (req, res, next) => {
     "email": "marvinpacis@example.com"
 } 
 */
-router.post("/", async (req, res, next) => {
+router.post("/", async (_req, res, next) => {
   res.json({ message: "template message" });
 });
 
 // DELETE: // http://localhost:3000/api/contacts/1
-router.delete("/:contactId", async (req, res, next) => {
+router.delete("/:contactId", async (_req, res, next) => {
   res.json({ message: "template message" });
 });
 
@@ -34,7 +34,7 @@ router.delete("/:contactId", async (req, res, next) => {
     "email": "shaw@example.com"
 } 
 */
-router.put("/:contactId", async (req, res, next) => {
+router.put("/:contactId", async (_req, res, next) => {
   res.json({ message: "template message" });
 });
 
