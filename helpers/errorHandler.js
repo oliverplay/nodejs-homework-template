581fbb4897939d062a0c7247902b52896d938590
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-09-12 16:42:12 +0800

    Update errorHandler.js

--- helpers/errorHandler.js
+++ helpers/errorHandler.js
@@ -1,12 +1,10 @@
-// helpers/errorHandler.js
-
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
 
   res.json({
     message: err.message,
-    // Optionally include stack trace in development
+    // Optionally include stack trace in development environment
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
