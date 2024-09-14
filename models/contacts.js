8f537eeb6764a697f6ae2bff6190d1898136bf62
Ruel Paya <ruel.paya@gmail.com>
2024-09-14 21:27:38 +0800

    add code
    auth.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,55 @@
-// const fs = require('fs/promises')
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleMongooseError } = require("../middleware/index");
 
-const listContacts = async () => {}
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const getContactById = async (contactId) => {}
+contactSchema.post("save", handleMongooseError);
 
-const removeContact = async (contactId) => {}
+const addSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string().min(8).max(12).required(),
+  favorite: Joi.boolean().required(false),
+});
 
-const addContact = async (body) => {}
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
 
-const updateContact = async (contactId, body) => {}
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+  Contact,
+  schemas,
+};
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+const { DB_HOST } = process.env;
+require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connect success !!!");
+    app.listen(3000, () => {
+      console.log(" Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("Database connect error 🙁", error.message);
+    process.exit(1);
+  });
