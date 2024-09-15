dcbce60abcb6d06befb1e938b3c51fd09c4cf84f
Ruel Paya <ruel.paya@gmail.com>
2024-09-15 14:46:49 +0800

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-
 const { User } = require("../models/user");
 const { HttpError } = require("../helpers");
 const { SECRET_KEY } = process.env;
@@ -8,20 +7,27 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer") {
-    next(HttpError(401));
+  // If no bearer token is provided, return 401
+  if (bearer !== "Bearer" || !token) {
+    return next(HttpError(401, "Unauthorized: No token provided"));
   }
+
   try {
+    // Verify the token and extract the user ID
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
+    // Check if user exists, has a token, and matches the provided token
     if (!user || !user.token || user.token !== token) {
-      next(HttpError(401));
+      return next(HttpError(401, "Unauthorized: Invalid token"));
     }
+
+    // Attach user object to the request and move to the next middleware
     req.user = user;
     next();
   } catch (error) {
-    next(HttpError(401));
+    // In case of JWT verification errors or other exceptions
+    return next(HttpError(401, "Unauthorized: Invalid token"));
   }
 };
 
