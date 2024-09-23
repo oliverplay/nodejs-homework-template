47d0c659ec57cf0d90b2b0dbb8865b06124da0d5
valeeriiiee <obleavalerie@gmail.com>
2024-09-23 14:41:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
-import contactsRouter from "./routes/api/contacts.js"; // Use ESM import for routes
+import router from "./routes/api/contactsRouter.js"; // Use ESM import for routes
 
 const app = express();
 
@@ -12,16 +12,14 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contactsRouter", router);
 
-// Handle 404 errors
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-// Handle other errors
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-export { app }; // Use default export
+export { app };
