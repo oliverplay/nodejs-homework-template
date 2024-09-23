5ea2d653dc4c4f47fb0539a3e15aea3ea13b0241
valeeriiiee <obleavalerie@gmail.com>
2024-09-23 14:26:45 +0800

    Create contactsModel.js

---
+++ models/contactsModel.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+// REFERENCE: https://mongoosejs.com/docs/api/schema.html
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: [true, "Set email for contact"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Set phone for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
+
+const Contact = model("contact", contactSchema);
+
+export { Contact };
