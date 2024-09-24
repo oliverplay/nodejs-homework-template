0e2263367ccc5080a13bb2826268b9a6240dc881
valeeriiiee <obleavalerie@gmail.com>
2024-09-24 13:19:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,29 @@ dotenv.config();
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
+if (!DB_HOST) {
+  throw new Error("DB_HOST environment variable is not defined.");
+}
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT, () =>
-      console.log(`Server running. Using our API on port: ${PORT}`)
-    );
-    console.log("Database connect successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Using our API on port: ${PORT}`);
+    });
+    console.log("Database connection successful");
   })
-  .catch((err) =>
-    console.log(`Server not running. Error message: ${err.message}`)
-  );
+  .catch((err) => {
+    console.error(`Database connection failed. Error: ${err.message}`);
+    process.exit(1); // Exit the process if unable to connect to the database
+  });
+
+process.on("unhandledRejection", (err) => {
+  console.error(`Unhandled Rejection: ${err.message}`);
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error(`Uncaught Exception: ${err.message}`);
+  process.exit(1);
+});
