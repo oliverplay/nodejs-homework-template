325a12f467cb7c8729ba6aa0fb837d3b6b74d3b0
valeeriiiee <obleavalerie@gmail.com>
2024-09-30 00:05:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import router from "./routes/api/contactsRouter.js"; // Import your routes correctly
+import { router as contactsRouter } from "./routes/api/contactsRouter.js";
 import { router as usersRouter } from "./routes/api/usersRouter.js";
 
 const app = express();
@@ -12,20 +12,20 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-// Use proper route for contacts API
-app.use("/api/contacts", router);
+// tells Express to serve static files from the public directory)
+// open http://localhost:3000/avatars/665c98dca10f7f28dc9eb8b2.jpeg on browser
+app.use(express.static("public"));
 
-// http://localhost:3000/api/users
+app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
 
-app.use((req, res) => {
+app.use((_req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-// Central error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message });
+app.use((err, _req, res, _next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 export { app };
