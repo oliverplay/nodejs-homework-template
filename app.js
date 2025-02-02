9a89ad53b0387954b9783401f566de9e92e27639
naomimariuta <157979968+naomimariuta@users.noreply.github.com>
2025-02-02 19:23:35 +0200

    first

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const usersRouter = require("./routes/api/users");
 
 const app = express();
 
+app.use(express.static("public"));
+
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
@@ -20,25 +22,25 @@ app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
-	res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: err.message });
+  res.status(500).json({ message: err.message });
 });
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
-	.connect(DB_HOST)
-	.then(() => {
-		console.log("Database connection succesful^_^");
-		app.listen(PORT, () => {
-			console.log(`Server running on port ${PORT}`);
-		});
-	})
-	.catch((error) => {
-		console.error("Database connection error:", error.message);
-	});
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection succesful^_^");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error.message);
+  });
 
 module.exports = app;
