d4112a29bfcebdaaf65371a9e71ce105842bda04
naomimariuta <157979968+naomimariuta@users.noreply.github.com>
2025-02-02 19:44:44 +0200

    avatar

--- models/user.js
+++ models/user.js
@@ -2,31 +2,35 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-	password: {
-		type: String,
-		required: [true, "Password is required"],
-	},
-	email: {
-		type: String,
-		required: [true, "Email is required"],
-		unique: true,
-	},
-	subscription: {
-		type: String,
-		enum: ["starter", "pro", "business"],
-		default: "starter",
-	},
-	token: {
-		type: String,
-		default: null,
-	},
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  avatarURL: {
+    type: String,
+    required: false,
+  },
 });
 
 userSchema.pre("save", async function (next) {
-	if (!this.isModified("password")) return next();
-	const salt = await bcrypt.genSalt(10);
-	this.password = await bcrypt.hash(this.password, salt);
-	next();
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
